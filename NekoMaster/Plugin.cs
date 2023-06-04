cbd4603eccae65cdee97189995f0b74684ebad8c
gamous <gammarayburst@163.com>
2023-06-04 20:42:29 +0800

    Add commandManager

--- NekoMaster/Plugin.cs
+++ NekoMaster/Plugin.cs
@@ -32,6 +32,7 @@ namespace NekoMaster
         {
             this.pi = pluginInterface;
             this.cmd = commandManager;
+            DalamudApi.Initialize(this,pi);
             pi.UiBuilder.OpenConfigUi += DrawConfigUI;
 
             this.Configuration = this.pi.GetPluginConfig() as Configuration ?? new Configuration();
@@ -48,10 +49,11 @@ namespace NekoMaster
             this.cmd.AddHandler("/load", new CommandInfo(OnCommand) { HelpMessage = "load plugin" });
             this.cmd.AddHandler("/unload", new CommandInfo(OnCommand) { HelpMessage = "unload plugin" });
             this.cmd.AddHandler("/reload", new CommandInfo(OnCommand) { HelpMessage = "reload plugin" });
-            this.cmd.AddHandler("/loadall", new CommandInfo(OnCommand) { HelpMessage =   "load all plugin" });
-            this.cmd.AddHandler("/unloadall", new CommandInfo(OnCommand) { HelpMessage = "unload all plugin" });
-            this.cmd.AddHandler("/reloadall", new CommandInfo(OnCommand) { HelpMessage = "reload all plugin" });
+            this.cmd.AddHandler("/loadall", new CommandInfo(OnCommand) { HelpMessage =   "load all plugins" });
+            this.cmd.AddHandler("/unloadall", new CommandInfo(OnCommand) { HelpMessage = "unload all plugins" });
+            this.cmd.AddHandler("/reloadall", new CommandInfo(OnCommand) { HelpMessage = "reload all plugins" });
             this.cmd.AddHandler("/errorfuck", new CommandInfo(OnCommand) { HelpMessage = "reset error" });
+            this.cmd.AddHandler("/unloadthd", new CommandInfo(OnCommand) { HelpMessage = "unload third plugins" });
         }
 
         //internal List<string> plugins = new List<string>();
@@ -79,7 +81,8 @@ namespace NekoMaster
                 var plugin = PluginQuery(name);
                 PluginLog.Log($"{plugin}");
                 bool isLoaded = (bool)plugin.GetFoP("IsLoaded");
-                if(isLoaded)
+                string pst = (string)plugin.GetFoP("State").ToString();
+                if (isLoaded &&!pst.Contains("Unloading"))
                 {
                     await (Task)plugin.GetType().GetMethod("UnloadAsync")?.Invoke(plugin, new object[] { false, true });
                 }
@@ -154,6 +157,18 @@ namespace NekoMaster
                     PluginUnload(pname);
             }
         }
+        public async void PluginUnloadThird()
+        {
+            dynamic plugins = PluginEnums();
+            foreach (object p in plugins)
+            {
+                string pname = (string)p.GetFoP("Name");
+                object manifest = p.GetFoP("Manifest");
+                bool isThird = (bool)manifest.GetFoP("IsThirdParty");
+                if (p != null && isThird && Name != pname )
+                    PluginUnload(pname);
+            }
+        }
         public async void PluginLoadAll()
         {
             dynamic plugins = PluginEnums();
@@ -184,9 +199,11 @@ namespace NekoMaster
             this.cmd.RemoveHandler("/unloadall");
             this.cmd.RemoveHandler("/reloadall");
             this.cmd.RemoveHandler("/errorfuck");
+            this.cmd.RemoveHandler("/unloadthd");
             this.WindowSystem.RemoveAllWindows();
             
             MainWindow.Dispose();
+            DalamudApi.Dispose();
         }
 
         private void OnCommand(string command, string args)
@@ -203,6 +220,7 @@ namespace NekoMaster
                 case "/unloadall": PluginUnloadAll(); break;
                 case "/reloadall": PluginReloadAll(); break;
                 case "/errorfuck": ErrorFuck(); break;
+                case "/unloadthd": PluginUnloadThird(); break;
                 default: ;break;
             }
         }
--- NekoMaster/Windows/MainWindow.cs
+++ NekoMaster/Windows/MainWindow.cs
@@ -1,8 +1,12 @@
 using System;
 using System.Collections.Generic;
 using System.Numerics;
+using System.Text;
 using System.Xml.Linq;
+using Dalamud.Game.Command;
 using Dalamud.Interface.Windowing;
+using Dalamud.Logging;
+using FFXIVClientStructs.FFXIV.Client.Game.Control;
 using ImGuiNET;
 using ImGuiScene;
 using NekoMaster.Reflection;
@@ -28,76 +32,164 @@ public class MainWindow : Window, IDisposable
     {
     }
 
+
+    private string filterName = string.Empty;
     public override void Draw()
     {
-        dynamic plugins = Plugin.PluginEnums();
-        ImGui.SameLine();
-        if (ImGui.Button($"ReloadAll"))
-        {
-            Plugin.PluginReloadAll();
-        }
-        ImGui.SameLine();
-        if (ImGui.Button($"LoadAll"))
-        {
-            Plugin.PluginLoadAll();
-        }
-        ImGui.SameLine();
-        if (ImGui.Button($"UnloadAll"))
-        {
-            Plugin.PluginUnloadAll();
-        }
-        ImGui.SameLine();
-        if (ImGui.Button($"ErrorFuck"))
-        {
-            Plugin.ErrorFuck();
-        }
-        ImGui.BeginTable("PluginManager",6);
-        ImGui.TableSetupColumn("Name", ImGuiTableColumnFlags.None, 4);
-        ImGui.TableSetupColumn("isOn",ImGuiTableColumnFlags.None,2);
-        ImGui.TableSetupColumn("isDev", ImGuiTableColumnFlags.None, 2);
-        ImGui.TableSetupColumn("isThird", ImGuiTableColumnFlags.None, 2);
-        ImGui.TableSetupColumn("State", ImGuiTableColumnFlags.None, 2);
-        ImGui.TableSetupColumn("Method", ImGuiTableColumnFlags.None, 6);
-        ImGui.TableHeadersRow();
+        ImGui.BeginTabBar("NekoNeko");
 
-        foreach (object p in plugins)
-        {
-            string pname = (string)p.GetFoP("Name");
-            string pstat = p.GetFoP("State").ToString();
-            object manifest = p.GetFoP("Manifest");
-            bool isDisabled = (bool)p.GetFoP("IsDisabled");
-            bool disabled = (bool)manifest.GetFoP("Disabled");
-            bool isDev = (bool)p.GetFoP("IsDev");
-            bool isThird = (bool)manifest.GetFoP("IsThirdParty");
-            ImGui.TableNextColumn();
-            ImGui.Text(pname);
-            ImGui.TableNextColumn();
-            ImGui.Text(isDisabled? "×" : "√");
-            ImGui.SameLine();
-            ImGui.Text(disabled ? "×" : "√");
-            ImGui.TableNextColumn();
-            ImGui.Text(isDev ? "√" : "×");
-            ImGui.TableNextColumn();
-            ImGui.Text(isThird ? "√" : "×");
-            ImGui.TableNextColumn();
-            ImGui.Text(pstat);
-            ImGui.TableNextColumn();
-            if (ImGui.Button($"Load##{pname}"))
-            {
-                Plugin.PluginLoad(pname);
-            };
-            ImGui.SameLine();
-            if (ImGui.Button($"Unload##{pname}")) {
-                Plugin.PluginUnload(pname);
-            };
-            ImGui.SameLine();
-            if (ImGui.Button($"Reload##{pname}"))
-            {
-                Plugin.PluginReload(pname);
-            };
-        }
-        ImGui.EndTable();
-        ImGui.Spacing();
+        if (ImGui.BeginTabItem("Plugins")) { 
+        
+            dynamic plugins = Plugin.PluginEnums();
 
+            if (ImGui.Button($"ReloadAll"))
+            {
+                Plugin.PluginReloadAll();
+            }
+            RightClickToCopyCmd($"/reloadall");
+            ImGui.SameLine();
+            if (ImGui.Button($"LoadAll"))
+            {
+                Plugin.PluginLoadAll();
+            }
+            RightClickToCopyCmd($"/loadall");
+            ImGui.SameLine();
+            if (ImGui.Button($"UnloadAll"))
+            {
+                Plugin.PluginUnloadAll();
+            }
+            RightClickToCopyCmd($"/unload");
+            ImGui.SameLine();
+            if (ImGui.Button($"ErrorFuck"))
+            {
+                Plugin.ErrorFuck();
+            }
+            RightClickToCopyCmd($"/errerfuck");
+            ImGui.SameLine();
+            if (ImGui.Button($"UnloadThird"))
+            {
+                Plugin.PluginUnloadThird();
+            }
+            RightClickToCopyCmd($"/unloadthd");
+            ImGui.SameLine();
+            ImGui.InputText("Search", ref filterName, 20, ImGuiInputTextFlags.None);
+            if (ImGui.BeginTable("PluginManager", 6, ImGuiTableFlags.ScrollY | ImGuiTableFlags.Resizable))
+            {
+                ImGui.TableSetupColumn("Name", ImGuiTableColumnFlags.None, 4);
+                ImGui.TableSetupColumn("isOn", ImGuiTableColumnFlags.None, 2);
+                ImGui.TableSetupColumn("isDev", ImGuiTableColumnFlags.None, 2);
+                ImGui.TableSetupColumn("isThird", ImGuiTableColumnFlags.None, 2);
+                ImGui.TableSetupColumn("State", ImGuiTableColumnFlags.None, 2);
+                ImGui.TableSetupColumn("Method", ImGuiTableColumnFlags.None, 6);
+                ImGui.TableHeadersRow();
+
+
+                foreach (object p in plugins)
+                {
+                    string pname = (string)p.GetFoP("Name");
+                    if (pname == null || !pname.Contains(filterName,StringComparison.OrdinalIgnoreCase)) continue;
+                    string pstat = p.GetFoP("State").ToString();
+                    object manifest = p.GetFoP("Manifest");
+                    bool isDisabled = (bool)p.GetFoP("IsDisabled");
+                    bool disabled = (bool)manifest.GetFoP("Disabled");
+                    bool isDev = (bool)p.GetFoP("IsDev");
+                    bool isThird = (bool)manifest.GetFoP("IsThirdParty");
+                    ImGui.TableNextColumn();
+                    ImGui.Text(pname);
+                    ImGui.TableNextColumn();
+                    ImGui.Text(isDisabled ? "×" : "√");
+                    ImGui.SameLine();
+                    ImGui.Text(disabled ? "×" : "√");
+                    ImGui.TableNextColumn();
+                    ImGui.Text(isDev ? "√" : "×");
+                    ImGui.TableNextColumn();
+                    ImGui.Text(isThird ? "√" : "×");
+                    ImGui.TableNextColumn();
+                    ImGui.Text(pstat);
+                    ImGui.TableNextColumn();
+                    if (ImGui.Button($"Load##{pname}"))
+                    {
+                        Plugin.PluginLoad(pname);
+                    };
+                    RightClickToCopyCmd($"/load {pname}");
+                    ImGui.SameLine();
+                    if (ImGui.Button($"Unload##{pname}"))
+                    {
+                        Plugin.PluginUnload(pname);
+                    };
+                    RightClickToCopyCmd($"/unload {pname}");
+                    ImGui.SameLine();
+                    if (ImGui.Button($"Reload##{pname}"))
+                    {
+                        Plugin.PluginReload(pname);
+                    };
+                    RightClickToCopyCmd($"/reload {pname}");
+                }
+                ImGui.EndTable();
+            }
+            //ImGui.Spacing();
+            ImGui.EndTabItem();
+        }
+        
+
+        if (ImGui.BeginTabItem("Commands")) {
+            ImGui.InputText($"##genic",ref CmdGicArg, 64);
+            ImGui.SameLine();
+            if (ImGui.Button("Dispatch"))
+            {
+                PluginLog.Log(CmdGicArg);
+                DalamudApi.CommandManager.ProcessCommand(CmdGicArg);
+            }
+            if (ImGui.BeginTable("CommandManager", 4, ImGuiTableFlags.ScrollY )) {
+                ImGui.TableSetupColumn("Name", ImGuiTableColumnFlags.None, 0.5f);
+                ImGui.TableSetupColumn("Args", ImGuiTableColumnFlags.None, 0.5f);
+                ImGui.TableSetupColumn("Asseambly", ImGuiTableColumnFlags.None,0.5f);
+                ImGui.TableSetupColumn("HelpMessage", ImGuiTableColumnFlags.None, 2);
+                ImGui.TableHeadersRow();
+                foreach (KeyValuePair<string, CommandInfo> cmd in DalamudApi.CommandManager.Commands)
+                {
+                    if (!CmdTempArg.ContainsKey(cmd.Key))
+                    {
+                        CmdTempArg.Add(cmd.Key, string.Empty);
+                    }
+                    ImGui.TableNextColumn();
+                    if (ImGui.Button(cmd.Key))
+                    {
+                        DalamudApi.CommandManager.ProcessCommand($"{cmd.Key} {CmdTempArg[cmd.Key]}");
+                    }
+                    RightClickToCopyCmd(cmd.Key);
+                    ImGui.TableNextColumn();
+                    var arg = CmdTempArg[cmd.Key];
+                    ImGui.InputText($"##{cmd.Key}", ref arg, 64);
+                    CmdTempArg[cmd.Key] = arg;
+                    ImGui.TableNextColumn();
+                    ImGui.Text((string)cmd.Value.GetFoP("LoaderAssemblyName"));
+                    ImGui.TableNextColumn();
+                    ImGui.Text(cmd.Value.HelpMessage);
+                }
+
+                ImGui.EndTable();
+            }
+            ImGui.EndTabItem();
+        }
+        
+
+        ImGui.EndTabBar();
+    }
+    private string CmdGicArg = string.Empty;
+    private Dictionary<string, string> CmdTempArg = new Dictionary<string, string>();
+
+
+    public void RightClickToCopyCmd(string cmd)
+    {
+        if (ImGui.IsItemHovered())
+        {
+            ImGui.SetTooltip($"Right-click to copy command:\n  {cmd}");
+        }
+
+        if (ImGui.IsItemHovered() && ImGui.IsItemClicked(ImGuiMouseButton.Right))
+        {
+            ImGui.SetClipboardText($"{cmd}");
+        }
     }
 }
